bd8be20c75c9512cced46d7a35a9e0c9cc597de9
Tartatamudo <fabianalarcon123@gmail.com>
2023-03-17 01:52:34 -0300

    Segundo commit

--- src/main.java
+++ src/main.java
@@ -1,5 +1,10 @@
 public class main {
-    public int SumaEnteros(int num1, int num2){
+    public static int SumaEnteros(int num1, int num2){
         return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        int x =SumaEnteros(5,10);
+        System.out.println(x);
     }
 }
